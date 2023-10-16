080f5d87f52b030ed2bdcdd4c359a68be1871ffd
AbdullahMazahreh <abdullah.mazahreh@gmail.com>
2023-10-16 17:36:25 +0300

    UPD

--- src/components/formHeading/FormHeading.jsx
+++ src/components/formHeading/FormHeading.jsx
@@ -1,14 +1,12 @@
-import React, { Fragment } from "react";
+import React from "react";
 import styles from "./formHeading.module.scss";
 
 function FormHeading({ heading, description }) {
   return (
-    <Fragment>
-      <div className={styles.headingWrapper}>
-        <h1 className={styles.headingWrapper_heading}>{heading}</h1>
-        <p className={styles.headingWrapper_description}>{description}</p>
-      </div>
-    </Fragment>
+    <div className={styles.headingWrapper}>
+      <h1 className={styles.headingWrapper_heading}>{heading}</h1>
+      <p className={styles.headingWrapper_description}>{description}</p>
+    </div>
   );
 }
 
